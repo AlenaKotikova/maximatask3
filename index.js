46f9d7548aeb1708c1087e7cd642d6c7dfefc572
AlenaKotikova <136328007+AlenaKotikova@users.noreply.github.com>
2023-10-01 16:23:09 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+function switchImages (offset) {
+    const activeSlide = document.querySelector('[data-active]');
+    
+    const slides = [...document.querySelectorAll('.slide')];
+    
+    const currentIndex = slides.indexOf(activeSlide);
+
+    let newIndex = currentIndex + offset;
+
+    if(newIndex < 0) newIndex = slides.length - 1;
+    if(newIndex >= slides.length) newIndex = 0;
+
+    slides[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active
+
+}
+
+const onNext = () => switchImages(1)
+const onPrev = () => switchImages(-1)
+
+
